1ade0c538f69fc78501656a5f06955d3c4f3c894
Luca Mussi <ing.luca.mussi@gmail.com>
2014-09-13 09:31:58 +0200

    famous wrappers to replace some templates among the ones from AT

--- package.js
+++ package.js
@@ -1,98 +1,40 @@
 Package.describe({
     summary: "Accounts Templates styled for Famo.us.",
-    version: "0.0.28",
+    version: "1.0.0",
     name: "splendido:accounts-templates-famous",
     git: "https://github.com/splendido/accounts-templates-famous.git",
 });
 
 Package.on_use(function(api, where) {
-    if (api.versionsFrom) {
-        api.use([
-            'service-configuration@1.0.0',
-            'accounts-base@1.0.0',
-            'splendido:accounts-templates-core@0.0.28',
-            'templating@1.0.4',
-            'less@1.0.5'
-        ], 'client');
-        api.use([
-            'service-configuration@1.0.0',
-            'accounts-password@1.0.0',
-            'accounts-base@1.0.0',
-            'splendido:accounts-templates-core@0.0.28',
-        ], 'server');
+    api.use([
+        'templating',
+        'gadicohen:famous-views',
+        'aldeed:template-extension',
+    ], 'client');
+    api.imply([
+        'splendido:accounts-templates-core@1.0.0',
+        'gadicohen:famous-views@0.1.6',
+        'aldeed:template-extension@2.0.0',
+    ], ['client', 'server']);
 
-        api.imply([
-            'splendido:accounts-templates-core@0.0.28',
-            'service-configuration',
-        ], ['client', 'server']);
-    }
-    else{
-        api.use([
-            'service-configuration',
-            'accounts-base',
-            'accounts-templates-core',
-            'templating',
-            'less'
-        ], 'client');
-        api.use([
-            'service-configuration',
-            'accounts-password',
-            'accounts-base',
-            'accounts-templates-core',
-        ], 'server');
-
-        api.imply([
-            'accounts-templates-core',
-            'service-configuration',
-        ], ['client', 'server']);
-    }
+    //api.use('splendido:accounts-templates-bootstrap@1.0.0', ['client', 'server'] /*, { weak: true } */);
+    //api.use('splendido:accounts-templates-foundation@1.0.0', ['client', 'server'] /*, { weak: true } */);
+    //api.use('splendido:accounts-templates-semantic-ui@1.0.0', ['client', 'server'] /*, { weak: true } */);
 
     api.add_files([
-        'lib/at_error.html',
-        'lib/at_error.js',
-        'lib/at_form.html',
-        'lib/at_form.js',
-        'lib/at_input.html',
-        'lib/at_input.js',
-        'lib/at_oauth.html',
-        'lib/at_oauth.js',
-        'lib/at_pwd_form.html',
-        'lib/at_pwd_form.js',
-        'lib/at_pwd_form_btn.html',
-        'lib/at_pwd_form_btn.js',
-        'lib/at_pwd_link.html',
-        'lib/at_pwd_link.js',
-        'lib/at_result.html',
-        'lib/at_result.js',
-        'lib/at_sep.html',
-        'lib/at_signin_link.html',
-        'lib/at_signin_link.js',
-        'lib/at_signup_link.html',
-        'lib/at_signup_link.js',
-        'lib/at_social.html',
-        'lib/at_social.js',
-        'lib/at_terms_link.html',
-        'lib/at_terms_link.js',
-        'lib/at_title.html',
-        'lib/at_title.js',
-        'lib/full_page_at_form.html',
-        'lib/at_famous.less'
+        'lib/at_famous_form.html',
+        'lib/at_famous_form.js',
+        'lib/at_famous_oauth.html',
+        'lib/at_famous_oauth.js',
+        'lib/at_famous_pwd_form.html',
+        'lib/at_famous_pwd_form.js',
     ], ['client']);
 });
 
 Package.on_test(function(api) {
-    if (api.versionsFrom) {
-        api.use([
-            'splendido:accounts-templates-bootstrap',
-            'splendido:accounts-templates-core@0.0.28',
-        ]);
-    }
-    else{
-        api.use([
-            'accounts-templates-bootstrap',
-            'accounts-templates-core',
-        ]);
-    }
+    api.use([
+        'splendido:accounts-templates-core@1.0.0',
+    ]);
     api.use(['tinytest', 'test-helpers'], ['client', 'server']);
     api.add_files('tests/accounts-templates-bootstrap_tests.js', ['client', 'server']);
 });
