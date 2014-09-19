4676b38f40ac80db09826ef210d11d0744b42224
Luca Mussi <ing.luca.mussi@gmail.com>
2014-09-19 15:22:05 +0200

    updated to work with 0.9.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Accounts Templates styled for Famo.us.",
-    version: "1.0.0",
+    version: "0.9.0",
     name: "splendido:accounts-templates-famous",
     git: "https://github.com/splendido/accounts-templates-famous.git",
 });
@@ -11,17 +11,17 @@ Package.on_use(function(api, where) {
         'templating',
         'gadicohen:famous-views',
         'aldeed:template-extension',
-        'splendido:accounts-templates-core@1.0.0',
+        'splendido:accounts-templates-core@0.9.0',
     ], 'client');
     api.imply([
-        'splendido:accounts-templates-core@1.0.0',
+        'splendido:accounts-templates-core@0.9.0',
         'gadicohen:famous-views@0.1.6',
         'aldeed:template-extension@2.0.0',
     ], ['client', 'server']);
 
-    api.use('splendido:accounts-templates-bootstrap@1.0.0', ['client', 'server'], { weak: true });
-    //api.use('splendido:accounts-templates-foundation@1.0.0', ['client', 'server'] /*, { weak: true } */);
-    //api.use('splendido:accounts-templates-semantic-ui@1.0.0', ['client', 'server'] /*, { weak: true } */);
+    api.use('splendido:accounts-templates-bootstrap@0.9.0', ['client', 'server'], { weak: true });
+    //api.use('splendido:accounts-templates-foundation@0.9.0', ['client', 'server'] /*, { weak: true } */);
+    //api.use('splendido:accounts-templates-semantic-ui@0.9.0', ['client', 'server'] /*, { weak: true } */);
 
     api.add_files([
         'lib/at_famous_form.html',
@@ -35,7 +35,7 @@ Package.on_use(function(api, where) {
 
 Package.on_test(function(api) {
     api.use([
-        'splendido:accounts-templates-core@1.0.0',
+        'splendido:accounts-templates-core@0.9.0',
     ]);
     api.use(['tinytest', 'test-helpers'], ['client', 'server']);
     api.add_files('tests/accounts-templates-bootstrap_tests.js', ['client', 'server']);
