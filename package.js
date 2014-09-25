09c275466fda43118c6c8d46a2581ee4d610c0e1
Luca Mussi <ing.luca.mussi@gmail.com>
2014-09-26 00:17:49 +0200

    more tests about animations

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Accounts Templates styled for Famo.us.",
-    version: "0.9.2",
+    version: "0.9.5",
     name: "splendido:accounts-templates-famous",
     git: "https://github.com/splendido/accounts-templates-famous.git",
 });
@@ -9,9 +9,9 @@ Package.on_use(function(api, where) {
     api.versionsFrom("METEOR@0.9.2.2");
 
     api.use([
-        'reactive-dict',
-        'templating',
-    ], 'client');
+        "reactive-dict",
+        "templating",
+    ], "client");
 
     api.use([
         "underscore",
@@ -21,31 +21,31 @@ Package.on_use(function(api, where) {
     ], ["client", "server"]);
 
     api.imply([
-        'splendido:accounts-templates-core@0.9.2',
-        'gadicohen:famous-views@0.1.7',
-        'aldeed:template-extension@2.0.0',
-    ], ['client', 'server']);
+        "splendido:accounts-templates-core@0.9.5",
+        "gadicohen:famous-views@0.1.7",
+        "aldeed:template-extension@2.0.0",
+    ], ["client", "server"]);
 
-    api.use('splendido:accounts-templates-unstyled@0.9.4', ['client', 'server'], { weak: true });
-    api.use('splendido:accounts-templates-bootstrap@0.9.4', ['client', 'server'], { weak: true });
-    api.use('splendido:accounts-templates-foundation@0.9.4', ['client', 'server'], { weak: true });
-    api.use('splendido:accounts-templates-semantic-ui@0.9.4', ['client', 'server'], { weak: true });
+    api.use("splendido:accounts-templates-unstyled", ["client", "server"], { weak: true });
+    api.use("splendido:accounts-templates-bootstrap", ["client", "server"], { weak: true });
+    api.use("splendido:accounts-templates-foundation", ["client", "server"], { weak: true });
+    api.use("splendido:accounts-templates-semantic-ui", ["client", "server"], { weak: true });
 
     api.add_files([
-        'lib/at_famous_form.html',
-        'lib/at_famous_form.js',
-        'lib/at_famous_oauth.html',
-        'lib/at_famous_oauth.js',
-        'lib/full_page_at_famous_form.html',
-        'lib/full_page_at_famous_form.js',
-        'lib/init.js',
-    ], ['client']);
+        "lib/at_famous_form.html",
+        "lib/at_famous_form.js",
+        "lib/at_famous_oauth.html",
+        "lib/at_famous_oauth.js",
+        "lib/full_page_at_famous_form.html",
+        "lib/full_page_at_famous_form.js",
+        "lib/init.js",
+    ], ["client"]);
 });
 
 Package.on_test(function(api) {
     api.use([
-        'splendido:accounts-templates-core@0.9.2',
+        "splendido:accounts-templates-core@0.9.5",
     ]);
-    api.use(['tinytest', 'test-helpers'], ['client', 'server']);
-    api.add_files('tests/tests.js', ['client', 'server']);
+    api.use(["tinytest", "test-helpers"], ["client", "server"]);
+    api.add_files("tests/tests.js", ["client", "server"]);
 });
