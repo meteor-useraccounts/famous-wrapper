659387760baf5a2c524baf94cdc716f896fe53ce
Luca Mussi <ing.luca.mussi@gmail.com>
2014-10-02 01:03:27 +0200

    Bump to version 0.0.4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Accounts Templates styled for Famo.us.",
-    version: "0.0.3",
+    version: "0.0.4",
     name: "splendido:accounts-templates-famous-wrapper",
     git: "https://github.com/splendido/accounts-templates-famous.git",
 });
@@ -21,30 +21,30 @@ Package.on_use(function(api, where) {
     ], ["client", "server"]);
 
     api.imply([
-        "splendido:accounts-templates-core@0.9.7",
-        "gadicohen:famous-views@0.1.9",
+        "splendido:accounts-templates-core@0.9.8",
+        "gadicohen:famous-views@0.1.10",
         "aldeed:template-extension@2.0.0",
     ], ["client", "server"]);
 
-    api.use("splendido:accounts-templates-unstyled@0.9.7", ["client", "server"], { weak: true });
-    api.use("splendido:accounts-templates-bootstrap@0.9.7", ["client", "server"], { weak: true });
-    api.use("splendido:accounts-templates-foundation@0.9.7", ["client", "server"], { weak: true });
-    api.use("splendido:accounts-templates-semantic-ui@0.9.7", ["client", "server"], { weak: true });
+    api.use("splendido:accounts-templates-unstyled@0.9.8", ["client", "server"], { weak: true });
+    api.use("splendido:accounts-templates-bootstrap@0.9.8", ["client", "server"], { weak: true });
+    api.use("splendido:accounts-templates-foundation@0.9.8", ["client", "server"], { weak: true });
+    api.use("splendido:accounts-templates-semantic-ui@0.9.8", ["client", "server"], { weak: true });
 
     api.add_files([
+        "lib/init.js",
         "lib/at_famous_form.html",
         "lib/at_famous_form.js",
         "lib/at_famous_oauth.html",
         "lib/at_famous_oauth.js",
         "lib/full_page_at_famous_form.html",
         "lib/full_page_at_famous_form.js",
-        "lib/init.js",
     ], ["client"]);
 });
 
 Package.on_test(function(api) {
     api.use([
-        "splendido:accounts-templates-core@0.9.7",
+        "splendido:accounts-templates-core@0.9.8",
     ]);
     api.use(["tinytest", "test-helpers"], ["client", "server"]);
     api.add_files("tests/tests.js", ["client", "server"]);
