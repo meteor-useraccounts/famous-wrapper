1679d9f470aef46029082bc03c32bc09219b4978
Luca Mussi <ing.luca.mussi@gmail.com>
2014-10-05 17:28:00 +0200

    Bump to version 0.1.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Famo.us wrapper for Accounts Templates.",
-    version: "0.1.0",
+    version: "0.1.1",
     name: "splendido:accounts-templates-famous-wrapper",
     git: "https://github.com/splendido/accounts-templates-famous.git",
 });
