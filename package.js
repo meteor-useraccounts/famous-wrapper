29527f181dc45682bfe0b4d0be34ee53ec6523fb
Luca Mussi <ing.luca.mussi@gmail.com>
2015-04-18 12:33:25 +0200

    back to version 1.8.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Famo.us wrapper for Accounts Templates.",
-    version: "1.9.0",
+    version: "1.8.1",
     name: "useraccounts:famous-wrapper",
     git: "https://github.com/meteor-useraccounts/famous-wrapper.git",
 });
